5fc92759c68ecf25526260c520c101910127abd7
1002 <2591190780@qq.com>
2025-10-27 16:29:20 +0800

    visulization

--- src/main/java/com/example/utils/Const.java
+++ src/main/java/com/example/utils/Const.java
@@ -13,7 +13,7 @@ public class Const {
     public static final  String CONTRACT_FOR_NFT_RULE = "0x59875722fa83b3b8b79c186dd1fdf5b07d452f3e";
     public static final  String CONTRACT_FOR_NFT_INFO_METHOD_STORENFTINFO = "storeNFTInfo";
     public static final  String CONTRACT_FOR_NFT_INFO_METHOD_TRADENFT = "tradeNFT";
-    public static final  String CONTRACT_FOR_NFT_INFO = "0xee0a61b0d8ebcabfd0037406b82b93f6ac66f2f8";
+    public static final  String CONTRACT_FOR_NFT_INFO = "0xef5b8e9b5b64588492649ceccccbce6504effbd2";
     public static final  String CONTRACT_FOR_MESSAGE_REPORT_METHOD_GETFULLEVIDENCEBYHASH = "getFullEvidenceByHash";
     public static final  String CONTRACT_FOR_MESSAGE_REPORT_METHOD_ADD_EVIDENCE = "addEvidence";
     public static final  String CONTRACT_FOR_MESSAGE_REPORT = "0x8331bc3d7d298ccb5c088f047344cab0a7ff9d7c";
